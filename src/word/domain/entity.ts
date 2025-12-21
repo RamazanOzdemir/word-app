49f5bcb72958e0661ef781ca78655d52cd3728a3
Ramazan Ozdemir <ramazanozdemir2607@gmail.com>
2025-12-21 22:44:30 +0100

    feat: Add Word Entity

---
+++ src/word/domain/entity.ts
@@ -0,0 +1,61 @@
+export type WordType = 'noun' | 'verb' | 'adjective';
+import { v4 as uuidv4 } from 'uuid';
+
+export class Word {
+  private constructor(
+    private readonly id: string,
+    private context: string,
+    private type: WordType,
+    private description: string,
+  ) {}
+
+  getId() {
+    return this.id;
+  }
+  getContext() {
+    return this.context;
+  }
+  getType() {
+    return this.type;
+  }
+  getDescription() {
+    return this.description;
+  }
+
+  static create(context: string, type: WordType, description: string) {
+    // Optional: business rule
+    if (!context.trim() || context.length < 1)
+      throw new Error('Context cannot be empty');
+
+    if (!description.trim() || description.length < 1)
+      throw new Error('Description cannot be empty');
+
+    const id = uuidv4();
+
+    return new Word(id, context, type, description);
+  }
+
+  updateContext(newContext: string) {
+    if (!newContext.trim()) throw new Error('Context cannot be empty');
+    this.context = newContext;
+  }
+
+  updateDescription(newDescription: string) {
+    if (!newDescription.trim()) throw new Error('Description cannot be empty');
+    this.description = newDescription;
+  }
+
+  updateType(newType: WordType) {
+    if (!newType) throw new Error('Word Type cannot be empty');
+    this.type = newType;
+  }
+
+  static fromPersistence(props: {
+    id: string;
+    context: string;
+    type: WordType;
+    description: string;
+  }) {
+    return new Word(props.id, props.context, props.type, props.description);
+  }
+}
